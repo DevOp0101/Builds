9895c33005a08f71d8dd98e9c06535ee9ed67957
DevOp0101 <142086016+DevOp0101@users.noreply.github.com>
2023-08-24 02:59:01 -0500

    Update Createinventory.php

--- Createinventory.php
+++ Createinventory.php
@@ -5,7 +5,7 @@ $inv = $_POST['inv'];
 
 
 
-$con = mysqli_connect("sql.freedb.tech:3306","freedb_DevOpData","MrNs%wzJ&CjDT9w", "freedb_DevOpUser") or ("Cannot connect!"  . mysql_error());
+$con = mysqli_connect("sql.freedb.tech:3306","freedb_DevOpUser","MrNs%wzJ&CjDT9w", "freedb_DevOpData") or ("Cannot connect!"  . mysql_error());
 if (!$con)
 	die('Could not connect: ' . mysql_error());
 	
